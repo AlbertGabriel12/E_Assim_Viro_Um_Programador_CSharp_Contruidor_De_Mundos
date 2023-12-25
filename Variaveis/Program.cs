86c45497e267721818d882a10ba86d2ebed80abb
AlbertGabriel12 <albertgabriel051@gmail.com>
2023-12-25 11:41:12 -0300

    variaveis

---
+++ Variaveis/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        long inteiro64Bits = 64;
+        int inteiro32Bits = 32;
+        short inteiro16Bits = 16;
+        byte inteiro8Bits = 8;
+
+        double flutuante64Bits = 64.64d;
+        float flutuante32Bits = 32.32f;
+
+        bool b = true;
+        char c = 'a';
+        string s = "I am String";
+    }
+}
